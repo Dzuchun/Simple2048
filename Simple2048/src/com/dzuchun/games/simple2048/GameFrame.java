e5987a36da9d3b74a1be1970cb5280d12fc39e5b
dzuchun <dzuchun@ZEPHYRKA-PF>
2019-12-09 21:15:18 +0200

    Released animation. No pixel issues.

--- Simple2048/src/com/dzuchun/games/simple2048/GameFrame.java
+++ Simple2048/src/com/dzuchun/games/simple2048/GameFrame.java
@@ -5,13 +5,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Toolkit;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.Vector;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
@@ -19,24 +15,24 @@ public class GameFrame extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 	private static int PLATE_SIZE = 40;
-	private static int ANIMATION_TIME_MILLIS = 1000;
+	private static long ANIMATION_TIME_MILLIS = 1000;
 	private static int FRAMES_PER_SECOND = 60;
 	
 	JLayeredPane canvas;
 	private int size;
 	JPanel mainPanel;
 	private Vector<GraphicalPlate> plates;
-	private AnimationThread animationThread;
-	boolean animationDrawn;
+	private Thread animationThread;
+	static final Object animationLock = new Object();
 	public GameFrame (int size)
 	{
 		this.size = size;
 		this.plates = new Vector<GraphicalPlate>(0);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setLayout(new BorderLayout());
-		
-		this.animationThread = new AnimationThread(this);
-		this.animationDrawn = false;
+		this.mainPanel = new JPanel();
+		this.mainPanel.setLayout(null);
+		this.add(this.mainPanel);
 		class GamePanel extends JLayeredPane
 		{
 			private static final long serialVersionUID = 1L;
@@ -44,7 +40,7 @@ public class GameFrame extends JFrame
 			public GamePanel()
 			{
 				super();
-				this.setSize((getPlateSize()+1)*size + 1, (getPlateSize()+1)*size + 1); //TODO append
+				this.setSize((PLATE_SIZE+1)*size + 1, (PLATE_SIZE+1)*size + 1); //TODO append
 			}
 			
 			@Override
@@ -54,33 +50,78 @@ public class GameFrame extends JFrame
 				g.setColor(Color.GRAY);
 				for (int i=0; i<=size; i++)
 				{
-					g.drawLine(0, (getPlateSize()+1)*i, (int)this.getSize().getWidth(), (getPlateSize()+1)*i); //TODO append
-					g.drawLine((getPlateSize()+1)*i, 0, (getPlateSize()+1)*i, (int)this.getSize().getHeight()); //TODO append
+					g.drawLine(0, (PLATE_SIZE+1)*i, (int)this.getSize().getWidth(), (PLATE_SIZE+1)*i); //TODO append
+					g.drawLine((PLATE_SIZE+1)*i, 0, (PLATE_SIZE+1)*i, (int)this.getSize().getHeight()); //TODO append
 				}
+				AbstractAnimation.drawAll(g, System.currentTimeMillis());
 				for (GraphicalPlate plate : plates)
 				{
-					plate.repaint();
+					plate.draw(g);
 				}
 				Toolkit.getDefaultToolkit().sync();
 			}
 		}
-		this.mainPanel = new JPanel();
-		this.mainPanel.setLayout(null);
-		this.add(this.mainPanel);
 		this.canvas = new GamePanel();
+		class AnimationThread extends Thread
+		{
+			public AnimationThread()
+			{
+				this.setDaemon(true);
+				this.setName("Animations-Thread");
+			}
+			@Override
+			public void run()
+			{
+				System.out.println("Started " + this.getName());
+				long currentTime;
+				long sleep;
+				long frameLength = 1000/FRAMES_PER_SECOND;
+				while(true)
+				{
+					currentTime = System.currentTimeMillis();
+					canvas.repaint(frameLength);
+					sleep = frameLength - (System.currentTimeMillis() - currentTime);
+					if (AbstractAnimation.allComplete())
+					{
+						try 
+						{
+							synchronized (animationLock) 
+							{
+								animationLock.wait();
+							}
+						}
+						catch (InterruptedException e) 
+						{
+							System.out.println("Unexpected exception:");
+							e.printStackTrace();
+						}
+					}
+					if (sleep > 0)
+					{
+						try 
+						{
+							Thread.sleep(sleep);
+						} 
+						catch (InterruptedException e) 
+						{
+							System.out.println("Unexpected exception:");
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+		this.animationThread = new AnimationThread();
+		this.animationThread.start();
 		this.mainPanel.add(this.canvas);
 		this.setSize(300, 300);
 		this.setVisible(true);
 	}
 	public void addPlate(int worth, Point pos)
 	{
-		//TODO check if plate exists
 		GraphicalPlate plate = new GraphicalPlate(worth);
 		plate.setPos(pos);
-		this.plates.add(plate);
-		this.canvas.add(plate);
-		this.canvas.setLayer(plate, 2);
-		this.repaint();
+		this.addPlate(plate);
 	}
 	public void addPlate(GraphicalPlate plate)
 	{
@@ -89,72 +130,27 @@ public class GameFrame extends JFrame
 		this.canvas.add(plate);
 		this.canvas.setLayer(plate, 2);
 		this.repaint();
-	}
-	public void performAnimation (int delay)
-	{
-		if (animationDrawn)
-		{
-			return;
-		}
-		this.animationThread.performAnimation(delay, GraphicalPlate.getScheduledPlates());
+		this.canvas.repaint();
+		plate.repaint(0);
 	}
 	public static int getPlateSize() 
 	{
 		return PLATE_SIZE;
 	}
-	public static void setPlateSize(int pLATE_SIZE) 
+	public static void drawAnimations()
 	{
-		PLATE_SIZE = pLATE_SIZE;
-	}
-	public static int getFRAMES_PER_SECOND() 
-	{
-		return FRAMES_PER_SECOND;
-	}
-	public static void setFRAMES_PER_SECOND(int fRAMES_PER_SECOND) 
-	{
-		FRAMES_PER_SECOND = fRAMES_PER_SECOND;
-	}
-	public static int getANIMATION_TIME_MILLIS() {
-		return ANIMATION_TIME_MILLIS;
-	}
-	public static void setANIMATION_TIME_MILLIS(int aNIMATION_TIME_MILLIS) 
-	{
-		ANIMATION_TIME_MILLIS = aNIMATION_TIME_MILLIS;
-	}
-	public void paint(Graphics g)
-	{
-		//TODO DEFINE
-	}
-}
-class AnimationThread extends Timer
-{
-	class DrawFrame extends TimerTask
-	{
-		@Override
-		public void run() 
+		System.out.println("Drawing animations!");
+		synchronized (animationLock) 
 		{
-			long currentTime = System.currentTimeMillis();
-			//TODO DEFINE
+			animationLock.notifyAll();
 		}
 	}
-	private GameFrame frame;
-	public AnimationThread(GameFrame frame)
+	public static long getAnimationTimeMillis() 
 	{
-		super(true);
-		this.frame = frame;
+		return ANIMATION_TIME_MILLIS;
 	}
-	private long millisStart;
-	private long millisEnd;
-	private long frameLength;
-	private Vector<GraphicalPlate> scheduledPlates;
-	@SuppressWarnings("unchecked")
-	public void performAnimation(int delay, Vector<GraphicalPlate> scheduledPlates)
+	public static void setAnimationTimeMillis(long aNIMATION_TIME_MILLIS) 
 	{
-		this.scheduledPlates = (Vector<GraphicalPlate>)scheduledPlates.clone();
-		this.millisStart = System.currentTimeMillis() + delay;
-		this.millisEnd = this.millisStart + GameFrame.getANIMATION_TIME_MILLIS() + delay;
-		this.frameLength = 1000/GameFrame.getFRAMES_PER_SECOND();
-		this.scheduleAtFixedRate(new DrawFrame(), delay, this.frameLength);
-		frame.animationDrawn = true;
+		ANIMATION_TIME_MILLIS = aNIMATION_TIME_MILLIS;
 	}
 }
--- Simple2048/src/com/dzuchun/games/simple2048/Testing.java
+++ Simple2048/src/com/dzuchun/games/simple2048/Testing.java
@@ -9,16 +9,28 @@ public class Testing {
 		ResourceLoader.load();
 		GameFrame a = new GameFrame(5);
 		GraphicalPlate b = new GraphicalPlate(2);
-		b.setPos(new Point(10, 10));
+		b.setPos(new Point(0, 1));
 		a.addPlate(b);
-		a.performAnimation(500);
+		try {
+			b.addAnimation(1000, new Point(42, 1));
+		} catch (IntersectsAnimationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println("1");
 		try {
 			Thread.sleep(4000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		a.performAnimation(500);
+		System.out.println("2");
+		try {
+			b.addAnimation(1000, new Point(42, 124));
+		} catch (IntersectsAnimationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
